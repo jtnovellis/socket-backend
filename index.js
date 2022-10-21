5f7d9cc0594b85773a0767015a92664a5ac46972
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2022-10-20 20:28:56 -0500

    asdf

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const io = socketIO(server, {
   cors: {
-    origin: 'https://sokeck-frontend.vercel.app/',
+    origin: 'http://localhost:3000',
     method: ['GET', 'POST'],
   },
 });
